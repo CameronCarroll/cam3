91b61d3a1662abda65a422eaa94cfa7f36ea7506
Cameron Carroll <ckcarroll4@gmail.com>
2011-11-28 05:10:33 -0800

    Initial import

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,27 @@
+class PageController < ApplicationController
+  layout 'application'
+  
+  def index
+    @title = "Homepage - Cameron Carroll"
+  end
+  
+  def rubyboot
+    @title = "Quick Start: Compiling Ruby and RVM"
+  end
+  
+  def docroot
+    @title = "Public Documents - Cameron Carroll"
+  end
+  
+  def texdocs
+    @title = "LaTeX Reference & Tutorials"
+  end
+  
+  def gamedev
+    @title = "Game Development & Design"
+  end
+  
+  def resources
+    @title = "Useful Resources"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cam3::Application.config.secret_token = '36a22864f111803d847260e9c20fd4fed8a6b890547a6b63549ff422b324e99c4bf8da6ab1bcd930ec38197b44d5c64f58502664f61d02eb1933d5f87f21635f'
